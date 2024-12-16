ec72b8cfc9dd89512eab69bea289d5cfe97caaeb
samuelrvld <samuelrvld@gmail.com>
2024-12-16 08:48:08 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function Index(){
+        return view('frontend.index');
+    }
+    public function ProfileStore(Request $request){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        $oldPhotoPath = $data->photo;
+
+        if ($request->hasFile('photo')){
+            $file = $request->file('photo');
+            $filename = time().'.'.$file->getClientOriginalExtension();
+            $file->move(public_path('upload/user_images'),$filename);
+            $data->photo = $filename;
+
+            if ($oldPhotoPath && $oldPhotoPath !== $filename){
+                $this->deleteOldImage($oldPhotoPath);
+            }
+        }
+        $data->save();
+        $notification = array(
+            'message' => 'Profile Updated Succes',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
+    private function deleteOldImage(string $oldPhotoPath): void{
+        $fullPath = public_path('upload/user_images/'.$oldPhotoPath);
+        if (file_exists($fullPath)){
+            unlink($fullPath);
+        }
+    }
+    public function UserLogout(){
+        Auth::guard('web')->logout();
+        return redirect()->route('login')->with('success','Logout Success');
+    }
+    public function ChangePassword(){
+        return view('frontend.dashboard.change_password');
+    }
+    public function UserPasswordUpdate(Request $request){
+        $user = Auth::guard('web')->user();
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed',
+        ]);
+
+        if (!Hash::check($request->old_password,$user->password)){
+            $notification = array(
+                'message' => 'Old Password Does not Match!',
+                'alert-type' => 'error'
+            );
+            return back()->with($notification);
+
+        }
+        //update new password
+        User::whereId($user->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+        $notification = array(
+            'message' => 'Password Change Success',
+            'alert-type' => 'success'
+        );
+        return back()->with($notification);
+
+    }
+}
---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <script>document.write(new Date().getFullYear())</script> © SAMUEL
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Uts Web Lanjut by <a href="#!" class="text-decoration-underline">SAMUEL RIVALDO SARAGIH</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
